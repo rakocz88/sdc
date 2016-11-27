3bc9f43d1e8b835cbe94ce55e85c68729d566786
filip <rakocz88@gmail.com>
2016-11-27 16:02:46 +0100

    'keke'

--- sdc-mail/src/main/java/com/pilaf/sdc/mail/SDCMailApp.java
+++ sdc-mail/src/main/java/com/pilaf/sdc/mail/SDCMailApp.java
@@ -2,12 +2,27 @@ package com.pilaf.sdc.mail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class SDCMailApp {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(SDCMailApp.class, args);
+
+	}
+
+	@Configuration
+	class ServerConfiguration implements EmbeddedServletContainerCustomizer {
+
+		private static final int MAIL_PORT = 9002;
+
+		@Override
+		public void customize(ConfigurableEmbeddedServletContainer server) {
+			server.setPort(MAIL_PORT);
+		}
 
 	}
 
